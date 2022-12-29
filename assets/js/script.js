6ad1c60f9978ce9353e07da33c30aec850cb20f3
erinsawyer504 <erinsawyer504@gmail.com>
2022-12-29 12:49:59 -0500

    adding HTML for quiz base

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,15 @@
 //add timer
 //add start button
 //add questions
-//code if a question is correct/incorrect and add/take points away
+//code if a question is correct/incorrect and add/take points away - take away time
 //add a place for the score
+//add a place to story initials
+
+//setting up variables
+var timerEl = document.querySelector(".timerCountDown");
+var startButton = document.querySelector(".startButton");
+var highScore = document.querySelector(".highScore");
+
+var score = 0;
+
+
